e41dead941d0948da389380e31dc91a93133d2ef
tmstafford <tatumstafford1@gmail.com>
2020-10-08 17:31:34 -0500

    variables and length prompt

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 // Assignment code here
 
+// password criteria variables
+var lowerCase = ("abcdefghijklmnopqrstuvwxyz");
+var upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+var numericChar = ("123456789");
+var specialChar = ("!#$%&'()*+,-./:;<=>?@[]^_|~");
+
+// User Prompts
+var length = window.prompt("How long do you want your password? Choose between 8 and 128 characters.");
+
+if (length < 8 || length > 128) {
+  window.alert("Your password must be between 8 and 128 characters. Please try again.");
+  return
+}
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
